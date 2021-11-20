108ed21c037ecb6f3eaec2351a868bda47107efd
Isabelle Arevalo <ia2422@columbia.edu>
2021-11-20 16:52:37 -0500

    set-freezer passes checkpatch

--- user/test/set-freezer/set-freezer.c
+++ user/test/set-freezer/set-freezer.c
@@ -9,24 +9,25 @@
 #define SCHED_FREEZER 7
 #endif
 
-/*Have not tested for with freezer since freezer implementation is not complete
-but works for other sched policies when run with sudo(can we fix that?)*/
+/*
+ * Have not tested for with freezer since freezer implementation is not complete
+ * but works for other sched policies when run with sudo(can we fix that?)
+ */
 
 int main(int argc, char **argv)
 {
-    if(argc != 2){
-        perror("Incorrect usage: ./set-freezer <pid>\n");
-        return -1;
-    }
+	if (argc != 2) {
+		perror("Incorrect usage: ./set-freezer <pid>\n");
+		return -1;
+	}
 
-    pid_t p = atoi(argv[1]);
-    struct sched_param sp = { .sched_priority = 0 };
+	pid_t p = atoi(argv[1]);
+	struct sched_param sp = { .sched_priority = 0 };
 
-    if((sched_setscheduler(p, SCHED_FREEZER, &sp)) < 0)
-    {
-        perror("Error in sched_setscheduler\n");
-        return -1;
-    }
-    printf("Policy set to freezer for process %d\n", p);
-    return 0;
+	if ((sched_setscheduler(p, SCHED_FREEZER, &sp)) < 0) {
+		perror("Error in sched_setscheduler\n");
+		return -1;
+	}
+	printf("Policy set to freezer for process %d\n", p);
+	return 0;
 }
