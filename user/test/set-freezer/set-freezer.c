995eda5349df978469afa96303a40bae43d573c1
Khyber Sen <kkysen@gmail.com>
2021-11-20 23:23:55 -0500

    Some minor error checking changes in set-freezer.

--- user/test/set-freezer/set-freezer.c
+++ user/test/set-freezer/set-freezer.c
@@ -3,31 +3,42 @@
 #include <unistd.h>
 #include <sched.h>
 #include <stdlib.h>
-#include "syscall.h"
+#include <limits.h>
 
 #ifndef SCHED_FREEZER
 #define SCHED_FREEZER 7
 #endif
 
-/*
+/**
  * Have not tested for with freezer since freezer implementation is not complete
- * but works for other sched policies when run with sudo(can we fix that?)
+ * but works for other sched policies when run with sudo (can we fix that?)
  */
-
-int main(int argc, char **argv)
+int main(const int argc, const char *const *const argv)
 {
 	if (argc != 2) {
-		perror("Incorrect usage: ./set-freezer <pid>\n");
-		return -1;
+		fprintf(stderr, "incorrect usage: %s <pid>\n", argv[0]);
+		return EXIT_FAILURE;
 	}
 
-	pid_t p = atoi(argv[1]);
-	struct sched_param sp = { .sched_priority = 0 };
+	char *endptr;
+	const long pid_long = strtol(argv[1], &endptr, 0);
 
-	if ((sched_setscheduler(p, SCHED_FREEZER, &sp)) < 0) {
-		perror("Error in sched_setscheduler\n");
-		return -1;
+	if (endptr[0] != 0
+		|| pid_long == LONG_MIN
+		|| pid_long == LONG_MAX
+		|| pid_long < 0) {
+		fprintf(stderr, "invalid pid: %s\n", argv[1]);
+		return EXIT_FAILURE;
 	}
-	printf("Policy set to freezer for process %d\n", p);
-	return 0;
+	const pid_t pid = (pid_t) pid_long;
+	const struct sched_param sp = {
+		.sched_priority = 0,
+	};
+
+	if ((sched_setscheduler(pid, SCHED_FREEZER, &sp)) == -1) {
+		perror("error in sched_setscheduler");
+		return EXIT_FAILURE;
+	}
+	printf("policy set to freezer for process %d\n", pid);
+	return EXIT_SUCCESS;
 }
